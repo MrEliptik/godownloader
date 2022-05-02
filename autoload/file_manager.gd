108e5a2c12ab3622cfdf00bcd2fa590bb0347ed5
mreliptik <victormeunier.dev@gmail.com>
2022-05-02 19:43:12 +0200

    get type from filename

--- autoload/file_manager.gd
+++ autoload/file_manager.gd
@@ -10,7 +10,8 @@ static func look_for_godot_in_dir(dir_path: String) -> Array:
 	var version_regex = RegEx.new()
 	version_regex.compile("((?:[0-9]\\.){1,2}[0-9])")
 	
-	#TODO: add regex for type (stable, alpha, etc..)
+	var type_regex = RegEx.new()
+	type_regex.compile("(?<=-)(.*?)(?=_)")
 	
 	if dir.open(dir_path) == OK:
 		dir.list_dir_begin()
@@ -26,10 +27,16 @@ static func look_for_godot_in_dir(dir_path: String) -> Array:
 					print(result.get_string())
 					var version_res = version_regex.search(file_name)
 					if version_res:
+						var type = ""
 						print(version_res.get_string())
+						var type_res = type_regex.search(file_name)
+						if type_res:
+							print(type_res.get_string())
+							type = type_res.get_string()
 						var version = {
 							"path":dir_path.plus_file(file_name),
-							"version": version_res.get_string()
+							"version": version_res.get_string(),
+							"type": type
 						}
 						res.append(version)
 			file_name = dir.get_next()
