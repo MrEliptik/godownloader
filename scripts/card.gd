8ffa438a2ae10305d4955b31fb9d4e7cf15ce3cb
mreliptik <victormeunier.dev@gmail.com>
2022-05-02 19:43:35 +0200

    update card to include version, path and type Add icons

--- scripts/card.gd
+++ scripts/card.gd
@@ -3,8 +3,16 @@ extends Control
 
 export var installed: bool = false setget set_installed
 
+var version: String = "x.x.x"
+var path: String = "path/to/somewhere"
+var type: String = ""
+
+onready var version_label = $ContentMargin/HBoxContainer/VBoxContainer/Version
+onready var path_label = $ContentMargin/HBoxContainer/VBoxContainer/Path
+
 func _ready() -> void:
-	
+	version_label.text = version + " - " + type
+	path_label.text = path
 	# Set values
 	$ContentMargin/HBoxContainer/DownloadBtn.visible = !installed
 	$ContentMargin/HBoxContainer/EraseBtn.visible = installed
