4f966f70ee4225b07bb8178b608810babba76b55
mreliptik <victormeunier.dev@gmail.com>
2022-05-13 00:08:43 +0200

    card_view

--- scripts/cards_view.gd
+++ scripts/cards_view.gd
@@ -6,9 +6,9 @@ onready var latest_version = $ContentMargin/VBoxContainer/LatestVersion
 onready var installed = $ContentMargin/VBoxContainer/Installed
 
 func _ready() -> void:
-	pass
-
-func _on_Setup_finish() -> void:
+	display_installed()
+	
+func display_installed() -> void:
 	for version in Globals.godot_versions:
 		var instance = card_scene.instance()
 		instance.path = version["path"]
@@ -16,3 +16,41 @@ func _on_Setup_finish() -> void:
 		instance.type = version["type"]
 		instance.installed = true
 		installed.add_child(instance)
+
+func check_new_version_available() -> void:
+	var latest_version = ""
+	var latest_version_arr = [0, 0, 0]
+	for version in Globals.available_versions:
+		if version["type"] != Globals.TYPE.STABLE: continue
+		var split = version["version"].split(".")
+		print(split)
+		for i in range(split.size()):
+			if int(split[i]) > latest_version_arr[i]:
+				latest_version_arr[i] = int(split[i])
+
+	
+	for i in range(latest_version_arr.size()):
+		if i == (latest_version_arr.size()-1):
+			latest_version += str(latest_version_arr[i])
+		else:
+			latest_version += str(latest_version_arr[i]) + "."
+			
+	print(latest_version)
+		
+	
+#	for version in Globals.godot_versions:
+#		if version["type"] != Globals.TYPE.STABLE: continue
+#		print(version["version"].split("."))
+
+func on_versions_updated() -> void:
+	pass
+#	check_new_version_available()
+
+func _on_Setup_finish() -> void:
+	display_installed()
+	
+	Globals.setup_complete = true
+	UserSettings.grab_settings()
+	UserSettings.save_settings()
+	
+	check_new_version_available()
