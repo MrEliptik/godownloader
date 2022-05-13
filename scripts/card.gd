40034e3d414f8c51dd9f1d3048a32c392c5f87bf
mreliptik <victormeunier.dev@gmail.com>
2022-05-14 00:10:33 +0200

    update card

--- scripts/card.gd
+++ scripts/card.gd
@@ -3,8 +3,8 @@ extends Control
 
 export var installed: bool = false setget set_installed
 
-var version: String = "x.x.x"
-var path: String = "path/to/somewhere"
+var version: String = "x.x.x" setget set_version
+var path: String = "path/to/somewhere" setget set_path
 var type: String = ""
 
 onready var version_label = $ContentMargin/HBoxContainer/VBoxContainer/Version
@@ -24,6 +24,16 @@ func set_installed(val: bool) -> void:
 		$ContentMargin/HBoxContainer/DownloadBtn.visible = !installed
 	if has_node("ContentMargin/HBoxContainer/EraseBtn"):
 		$ContentMargin/HBoxContainer/EraseBtn.visible = installed
+		
+func set_version(new_version: String) -> void:
+	version = new_version
+	if version_label:
+		version_label.text = version
+		
+func set_path(new_path: String) -> void:
+	path = new_path
+	if path_label:
+		path_label.text = path
 		
 func launch(path: String) -> void:
 	var output = []
